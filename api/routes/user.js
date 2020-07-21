22d0962a031cfab4ec624ed747a27086dc226fa9
Pratik Patil <pratik-cakesofttech.com>
2020-07-21 18:07:34 +0530

    Access Token Checked for OTP Operations

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,8 +8,6 @@ router.post('/userCreate', UserController.userCreate);
 router.use('/userLogin', UserController.userLogin);
 router.post('/userResetPassword', UserController.userResetPassword);
 router.post('/userNewPassword', UserController.userNewPassword);
-router.post('/sendOTP', UserController.sendOTP);
-router.post('/verifyOTP', UserController.verifyOTP);
 router.post('/addReferral', UserController.addReferral);
 
 // with auth
@@ -17,6 +15,8 @@ router.get('/userMe', UserController.verifyToken, UserController.userMe);
 router.post('/userChangePassword', UserController.verifyToken, UserController.userChangePassword);
 router.post('/userLogout', UserController.verifyToken, UserController.userLogout);
 router.post('/addWalletAddress', UserController.verifyToken, UserController.addWalletAddress);
+router.post('/sendOTP', UserController.verifyToken,  UserController.sendOTP);
+router.post('/verifyOTP', UserController.verifyToken, UserController.verifyOTP);
 router.post('/referralStatusUpdate', UserController.verifyToken, UserController.referralStatusUpdate);
 
 module.exports = router;
